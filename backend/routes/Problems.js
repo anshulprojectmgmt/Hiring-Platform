4adb38abe95c8111f105f0fe97b6b981cbf2e54e
sumit-realtyai <sumit@realtyai.net>
2024-08-14 09:40:38 +0530

    cmnted OPEN CHAT API Code

--- backend/routes/Problems.js
+++ backend/routes/Problems.js
@@ -5,8 +5,8 @@ const ObjectId = mongoose.Types.ObjectId;
 
 const idsArray = [
   new ObjectId('66089589338be786d245c01a'),
-  new ObjectId('660895a1338be786d245c045'),
-  new ObjectId('66089589338be786d245c021'),
+  new ObjectId('660895a1338be786d245c038'),
+  new ObjectId('660895a1338be786d245c033'),
   
   // Add more ObjectId instances as needed
   
