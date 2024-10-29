ace226b186564da087434c5268a112bca1295586
sumit-realtyai <sumit@realtyai.net>
2024-10-29 18:03:29 +0530

    speech api & domain update

--- backend/routes/speech-to-text.js
+++ backend/routes/speech-to-text.js
@@ -1,47 +1,47 @@
-// // Import the AssemblyAI client
-// const {AssemblyAI} = require('assemblyai');
-// console.log('assemblyAI:' , typeof AssemblyAI)
+// Import the AssemblyAI client
+const {AssemblyAI} = require('assemblyai');
+console.log('assemblyAI:' , typeof AssemblyAI)
 
-// const express = require("express");
-// const router = express.Router();
-// const Candidate = require("../models/Candidates");
+const express = require("express");
+const router = express.Router();
+const Candidate = require("../models/Candidates");
 
-// // Initialize the client with your API key
-// const client = new AssemblyAI({
-//   apiKey: '79c636b94ddb416688ea6c6c83818057', // Replace with your actual API key
-// });
+// Initialize the client with your API key
+const client = new AssemblyAI({
+  apiKey: '79c636b94ddb416688ea6c6c83818057', // Replace with your actual API key
+});
 
-// // Remote file URL for transcription
-// const FILE_URL ='https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/agrawal.krush.che22%40itbhu.ac.in-subjVideo-1.mp4'
-// // 'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/abhikdey.official%40gmail.com-subjVideo-1.mp4'
-// // 'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/sangeeta.you%40gmail.com-subjVideo-1.mp4'
-// // 'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/agrawal.krush.che22%40itbhu.ac.in-subjVideo-2.mp4'
-// //  'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/yashussrao%40gmail.com-subjVideo-1.mp4'
-// //  'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/sonibhaya999%40gmail.com-subjVideo-2.mp4'
-// // 'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/sonibhaya999%40gmail.com-subjVideo-2.mp4';
+// Remote file URL for transcription
+const FILE_URL ='https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/agrawal.krush.che22%40itbhu.ac.in-subjVideo-1.mp4'
+// 'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/abhikdey.official%40gmail.com-subjVideo-1.mp4'
+// 'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/sangeeta.you%40gmail.com-subjVideo-1.mp4'
+// 'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/agrawal.krush.che22%40itbhu.ac.in-subjVideo-2.mp4'
+//  'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/yashussrao%40gmail.com-subjVideo-1.mp4'
+//  'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/sonibhaya999%40gmail.com-subjVideo-2.mp4'
+// 'https://hm-video-audio-bucket.s3-accelerate.amazonaws.com/pRNbRDG543qkrZr/Videos/sonibhaya999%40gmail.com-subjVideo-2.mp4';
 
-// // Request parameters
-// const data = {
-//   audio: FILE_URL,  // You can also provide a local file path or a stream
-// };
+// Request parameters
+const data = {
+  audio: FILE_URL,  // You can also provide a local file path or a stream
+};
 
-// // Function to handle transcription
-// const speechToText = async (req, res) => {
-//     console.log('req received speech')
-//   try {
-//     // Submit the transcription request
-//     const transcript = await client.transcripts.transcribe(data);
+// Function to handle transcription
+const speechToText = async (req, res) => {
+    console.log('req received speech')
+  try {
+    // Submit the transcription request
+    const transcript = await client.transcripts.transcribe(data);
 
-//     // Output the transcription result
-//     console.log(transcript.text);
-//     return res.json({speech: transcript.text})
-//   } catch (error) {
-//     // Handle any errors
-//     console.error('Error during transcription:', error.message);
-//     return res.status(500).json({speech: transcript.text})
-// }
-// };
+    // Output the transcription result
+    console.log(transcript.text);
+    return res.json({speech: transcript.text})
+  } catch (error) {
+    // Handle any errors
+    console.error('Error during transcription:', error.message);
+    return res.status(500).json({speech: transcript.text})
+}
+};
 
-// router.post('/speech-to-text', speechToText)
+router.post('/speech-to-text', speechToText)
 
-// module.exports = router
+module.exports = router
